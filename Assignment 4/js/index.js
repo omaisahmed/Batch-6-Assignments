a21975527d2e738f6cf08e6a39b017339240d622
Omais Ahmed <58178722+omaisahmed@users.noreply.github.com>
2022-09-17 02:02:08 +0500

    Assignment 4
    Assignment 4

---
+++ Assignment 4/js/index.js
@@ -0,0 +1,86 @@
+alert("Developers Academy");
+
+// JavaScript Variables
+
+var text = "Developers Academy";
+console.log(text);
+
+// JavaScript Arithmetic Operators
+
+// Addition
+var x = 5;
+var y = 6;
+var z = x + y;
+console.log("The value of z is: " + z);
+
+// Subtraction
+var a = 10;
+var b = 6;
+var c = a + b;
+console.log("The value of c is: " + c);
+
+// Multiplication
+var q = 88;
+var w = 23;
+var e = q * w;
+console.log("The value of e is: " + e);
+
+// Division
+var r = 20;
+var t = 2;
+var u = r / t;
+console.log("The value of u is: " + u);
+
+// Modulus
+var l = 10;
+var m = 3;
+var j = l % m;
+console.log("The value of j is: " + j);
+
+// Concatenation
+var text1 = "Omais";
+var text2 = "Ahmed";
+var text3 = text1 + " " + text2;
+console.log("My name is " + text3);
+
+// Concatenation Another Example
+var string1 = "What a very ";
+string1 += "nice day";
+console.log(string1);
+
+// Adding Strings and Numbers
+var x1 = 5 + 5;
+console.log(x1);
+var x2 = "5" + 5;
+console.log(x2);
+var x3 = "Hello" + 5;
+console.log(x3);
+
+// Pre Increment operation
+var pi1 = 6;
+var pre_increment = ++pi1;
+console.log(pre_increment);
+
+// Post Increment
+var pi2 = 9;
+var post_increment = pi2++;
+console.log(post_increment);
+
+// Pre Decrement 
+var pd1 = 20;
+var pre_decrement = pd1++;
+console.log(pre_decrement);
+
+// Post Decrement 
+var pd2 = 24;
+var post_decrement = pd2++;
+console.log(post_decrement);
+
+// Math Expressions with BODMAS
+var exp1 = 5;
+var exp2 = 4;
+var exp3 = 8;
+var exp4 = 26;
+console.log(exp1++ + --exp4 - exp2++ * --exp3);
+
+
