5c23268f992a558c9663aa965736e69abc63acb6
Omais Ahmed <58178722+omaisahmed@users.noreply.github.com>
2022-10-03 01:46:31 +0500

    Assignment 14
    Assignment 14

---
+++ Assignment 14/js/index.js
@@ -0,0 +1,69 @@
+// Perform all these methods with example?
+
+// 1) new Date();
+var date = new Date();
+console.log("Date: " + date);
+
+// 2) getDay();
+var day = new Date().getDay();
+console.log("Day: " + day);
+
+// 3) getMonth();
+var month = new Date().getMonth();
+console.log("Month: " + month);
+
+// 4) getDate();
+var getDay = new Date().getDate();
+console.log("Get Day: " + getDay);
+
+// 5) getFullYear();
+var getFullYear = new Date().getFullYear();
+console.log("Get Full Year: " + getFullYear);
+
+// 6) getHours();
+var getHours = new Date().getHours();
+console.log("Get Hours: " + getHours);
+
+// 7) getMinutes();
+var getMinutes = new Date().getMinutes();
+console.log("Get Minutes: " + getMinutes);
+
+// 8) getSeconds();
+var getSeconds = new Date().getSeconds();
+console.log("Get Seconds: " + getSeconds);
+
+// 9) getMiliSeconds();
+var getMiliSeconds = new Date().getMilliseconds();
+console.log("Get Milliseconds: " + getMiliSeconds);
+
+// 10) getTime();
+var getTime = new Date().getTime();
+console.log("Get Time: " + getTime);
+
+// 11) setFullYear();
+var setFullYear = new Date().setFullYear(2022);
+console.log("Set Full Year: " + setFullYear);
+
+// 112) setMonth();
+var setMonth = new Date().setMonth(4);
+console.log("Set Month: " + setMonth);
+
+// 13) setDate();
+var setDate = new Date().setDate(16);
+console.log("Set Date: " + setDate);
+
+// 14) setHours();
+var setHours = new Date().setHours(15);
+console.log("Set Hours: " + setHours);
+
+// 15) setMinutes();
+var setMinutes = new Date().setMinutes(17);
+console.log("Set Minutes: " + setMinutes);
+
+// 16) setSeconds();
+var setSeconds = new Date().setSeconds(35);
+console.log("Set Seconds: " + setSeconds);
+
+// 17) setMiliSeconds();
+var setMiliSeconds = new Date().setMilliseconds(192);
+console.log("Set MiliSeconds: " + setMiliSeconds);
