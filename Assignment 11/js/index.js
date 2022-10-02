5b31fa79be40d0fea290a4a41c1eccc3e2795138
Omais Ahmed <58178722+omaisahmed@users.noreply.github.com>
2022-10-02 23:43:10 +0500

    Assignment 11
    Assignment 11

---
+++ Assignment 11/js/index.js
@@ -0,0 +1,42 @@
+// Perform all these methods with example?
+
+// 1) Math.round()
+console.log(Math.round(4.1));
+
+// 2) Math.ceil()
+console.log(Math.ceil(7.8));
+
+// 3) Math.floor()
+console.log(Math.floor(5.8));
+
+// 4) Math.random()
+console.log(Math.random());
+
+// 5) parseInt()
+console.log(parseInt("10", 10));
+
+// 6) parseFloat()
+console.log(parseFloat(123.345));
+
+// 7) toString()
+var num = 15;
+var no = num.toString();
+console.log(no);
+
+// 8) Number()
+console.log(Number(true));
+
+// 9) toFixed()
+var number = 5.56789;
+var n = num.toFixed();
+console.log(n);
+
+// 10) toUpperCase()
+var str = "ga altaf bhai!";
+var res = str.toUpperCase();
+console.log(res);
+
+// 11) toLowerCase()
+var string = "ye bik gayi ha government";
+var response = string.toLowerCase();
+console.log(response);
